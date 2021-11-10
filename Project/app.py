f7fe8aac77f67ba25b87bb69e19046f37eb8f1a1
Mohitu9403 <pdc.geca@gmail.com>
2021-11-10 13:01:52 +0530

    initial commit

---
+++ Project/app.py
@@ -0,0 +1,33 @@
+# Imports 
+
+from typing import List 
+from flask import Flask, render_template, request
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+#chatbot = ChatBot('Training Example')
+#trainer = ListTrainer(chatbot)
+
+
+
+app = Flask(__name__, template_folder="template")
+#create chatbot
+english_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
+trainer = ChatterBotCorpusTrainer(english_bot)
+trainer.train("chatterbot.corpus.english") #train the chatter bot for english
+
+#define app routes
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/get")
+
+
+#function for the bot response
+def get_bot_response():
+    userText = request.args.get('msg')
+    return str(english_bot.get_response(userText))
+
+
+if __name__ == "__main__":
+    app.run()
